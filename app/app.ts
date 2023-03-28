da17e938068f7ffdb5474f90719377f91e24c0b1
Jacob Thomas <jacgretho@gmail.com>
2023-03-28 15:10:41 -0500

    WIP

--- app/app.ts
+++ app/app.ts
@@ -1,3 +1,6 @@
+/// <reference path="player.ts" />
+
+
 function startGame() {
     // starting a new game
 
@@ -12,16 +15,6 @@ function logPlayer(name: string = 'MultiMath Player'): void {
     console.log(`New game starting for player: ${name}`);
 }
 
-function getInputValue(elementID: string): string | undefined {
-    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
-
-    if (inputElement.value === '') {
-        return undefined;
-    }
-    else {
-        return inputElement.value;
-    }
-}
 
 function postScore(score: number, playerName: string = 'MultiMath Player'): void {
 
